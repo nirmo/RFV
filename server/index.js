ebe70db41b0f9384048be57233186578e5556c54
nirmo <nirmoshe711@gmail>
2023-09-04 10:42:00 +0200

    upload_files

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const http = require ("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://192.168.2.6:3000",
+    },
+});
+
+io.on("connection", (socket) => {
+    io.to(socket.id).emit("from_backend", "RealFace");
+
+    socket.on("from_frontend", (data) => {
+        io.to(socket.id).emit("from_backend", data);
+    });
+});
+
+server.listen(3001, () => {
+    console.log("Listening on port 3001...");
+});
