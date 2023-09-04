04d04e4176d2651ccc1fdd7c22878de0c31376b1
nirmo <nirmoshe711@gmail>
2023-09-04 10:46:34 +0200

    upload client

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import io from 'socket.io-client'
+import { useEffect, useState } from "react"
+
+const socket = io.connect("http://192.168.2.6:3001");
+
+function App() {
+  const [messageReceived, setMessageReceived] = useState("");
+  const sendMessage = () => {
+    socket.emit("from_frontend", "RealFaceValue");
+  };
+
+  useEffect(() => {
+    socket.on("from_backend", (data) => {
+      setMessageReceived(data);
+    });
+  });
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      <button onClick={sendMessage}>Send Message</button>
+      <h1>{messageReceived}</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
